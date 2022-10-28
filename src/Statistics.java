51b3285474ea684d53460871eca2bd5a2d0c84dc
ParkChanhee1130 <kkazza1130@gmail.com>
2022-10-28 15:19:11 +0900

    first commit

---
+++ src/Statistics.java
@@ -0,0 +1,8 @@
+public class Statistics {
+    public void Statistic() {
+    // 통계
+    System.out.println("통계");
+    // 통계 출력
+
+    }
+}
---
+++ src/Surveys.java
@@ -0,0 +1,9 @@
+public class Surveys {
+    public void Survey() {
+    // 설문 시작
+    System.out.println("설문 시작");
+    // 이름과 휴대폰 입력 확인
+    // 설문과 답항 내용 출력
+    // 설문자 답 받기
+    }
+}
